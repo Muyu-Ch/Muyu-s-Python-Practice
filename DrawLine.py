180725eed1b8edc716a0af176bce3926bd31be1c
muyu <1744391048@qq.com>
2026-03-08 16:18:45 +0800

    修改了对于垂直线的绘画bug 增加了多条线的绘画

--- DrawLine.py
+++ DrawLine.py
@@ -1,7 +1,7 @@
-def show_points(point,x,y):
+def show_points(point,x,y):#draw the points
     width=3
     for i in range(x):
-        for j in range(y):
+        for j in range(y):#遍历所有坐标检测（x，y）是否在集合内
             if i==0:
                 print(f'{j:>{width}}',end="")
             elif j==0:
@@ -12,7 +12,7 @@ def show_points(point,x,y):
                 print(f'{" ":>{width}}',end='')
         print("")
 
-def show(points):
+def show(points):#灵活选取画布大小
     x=0
     y=0
     for point in points:
@@ -23,7 +23,9 @@ def show(points):
 def line(point1,point2):
     points=[]
     if point1[0]==point2[0]:
-        for i in range(point1[1],point2[1]+1):
+        max_y=max(point1[1],point2[1])
+        min_y=min(point1[1],point2[1])
+        for i in range(min_y,max_y+1):
             points.append([point1[0],i])
         return points
     a=(point1[1]-point2[1])/(point1[0]-point2[0])
@@ -36,9 +38,18 @@ def line(point1,point2):
             points.append([i,int(a*(i-min_x)+point2[1])])
     return points
 
+def lines(points1,points2):
+    points=[]
+    for i in range(len(points1)):
+        points+=line(points1[i],points2[i])
+    return points
+
+
 p1=[int(input("X of the first point: ")),int(input("Y of the first point: "))]
 p2=[int(input("X of the second point: ")),int(input("Y of the second point: "))]
-ps=line(p1,p2)
+p3=[int(input("X of the third point: ")),int(input("Y of the third point: "))]
+p4=[int(input("X of the fourth point: ")),int(input("Y of the fourth point: "))]
+ps=lines([p1,p3],[p2,p4])
 show(ps)
 
 
