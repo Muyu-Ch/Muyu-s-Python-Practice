9a5b534205fd3de6e5f9380be8239d9d86db08ab
muyu <1744391048@qq.com>
2026-03-08 17:40:28 +0800

    修改了代码结构

--- DrawLine.py
+++ DrawLine.py
@@ -1,26 +1,23 @@
-def show_points(point,x,y):#draw the points
-    width=3
-    for i in range(x):
-        for j in range(y):#遍历所有坐标检测（x，y）是否在集合内
-            if i==0:
-                print(f'{j:>{width}}',end="")
-            elif j==0:
-                print(f'{i:>{width}}',end="")
-            elif [i,j] in point:
-                print(f'{"*":>{width}}',end='')
-            else:
-                print(f'{" ":>{width}}',end='')
-        print("")
-
-def show(points):#灵活选取画布大小
+def show(points):#绘画所有的点
     x=0
     y=0
     for point in points:
         x=max(x,point[0])
         y=max(y,point[1])
-    show_points(points,x+1,y+1)
+    width = 3
+    for i in range(x):
+        for j in range(y):  # 遍历所有坐标检测（x，y）是否在集合内
+            if i == 0:
+                print(f'{j:>{width}}', end="")
+            elif j == 0:
+                print(f'{i:>{width}}', end="")
+            elif [i, j] in point:
+                print(f'{"*":>{width}}', end='')
+            else:
+                print(f'{" ":>{width}}', end='')
+        print("")
 
-def line(point1,point2):
+def line(point1,point2):#给定两个点，画出一条线
     points=[]
     if point1[0]==point2[0]:
         max_y=max(point1[1],point2[1])
@@ -38,18 +35,30 @@ def line(point1,point2):
             points.append([i,int(a*(i-min_x)+point2[1])])
     return points
 
-def lines(points1,points2):
+'''线表结构：ls:
+    [l1:[[x11,y11],[x12,y12]]
+     l2:[[x21,y21],[x22,y22]]
+     l3:[[x31,y31],[x32,y32]]
+     ......
+     ]'''
+
+def lines(ls):#连续画出好几条线
     points=[]
-    for i in range(len(points1)):
-        points+=line(points1[i],points2[i])
+    for i in range(len(ls)):
+        points+=line(ls[i][0],ls[i][1])
     return points
 
+n=int(input("the number of lines: "))
 
-p1=[int(input("X of the first point: ")),int(input("Y of the first point: "))]
-p2=[int(input("X of the second point: ")),int(input("Y of the second point: "))]
-p3=[int(input("X of the third point: ")),int(input("Y of the third point: "))]
-p4=[int(input("X of the fourth point: ")),int(input("Y of the fourth point: "))]
-ps=lines([p1,p3],[p2,p4])
+ls=[]
+
+for i in range(n):
+    ls.append([
+        [int(input(f"X of the first point of num{i+1} line: ")),int(input(f"Y of the first point of num{i+1} line: "))],
+        [int(input(f"X of the second point of num{i+1} line: ")),int(input(f"Y of the second point of num{i+1} line: "))]
+    ])
+
+ps=lines(ls)
 show(ps)
 
 
